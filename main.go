7d2c586cac7f332c5c609d4cfb596e083bf11277
Harsh97x <Rynzie@protonmail.com>
2023-06-20 19:38:26 +0530

    refactored the whole file

--- main.go
+++ main.go
@@ -6,36 +6,39 @@ import (
 	"fmt"
 )
 
-func main() {
-	type Block struct {
-		Hash     []byte
-		Data     []byte
-		Prevhash []byte
-	}
-
-	type Blockchain struct {
-		blocks []*Block
-	}
-
-	func (b *Block) derivehash () {
-		info := bytes.Join([][]byte{b.Data, b.Prevhash}, []byte{})
-		hash := sha256.Sum256(info)
-		b.hash = hash[:]
-	}
-
-	func CreateBlock(data string, prevHash []byte) *Block {
-		block := &Block{[]byte{}, []byte(data), prevHash}
-		block.DeriveHash()
-		return block
-	}
-
-	func (chain *Blockchain) AddBlock (data string) {
-		prevblock := chain.blocks[len(chain.block-1)]
-		new := Createblock(data, prevblock)
-		chain.blocks := append(chain.blocks, new)
-	}
-
-	func Genesis() *Block {
-		return CreateBlock("Genesis", []byte{})
-	} 
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	Prevhash []byte
 }
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+func (b *Block) DeriveHash () {
+	info := bytes.Join([][]byte{b.Data, b.Prevhash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+    prevBlock := chain.blocks[len(chain.blocks)-1]
+    new := CreateBlock(data, prevBlock.Hash)
+    chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+    return &BlockChain{[]*Block{Genesis()}}
+}
+
